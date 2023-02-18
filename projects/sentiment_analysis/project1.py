0554b4fc97b8de89abdd91b560ed448c6eba7f08
Beatriz Negreiros (IWS) <beatriznegreiros@outlook.com>
2023-02-18 14:26:33 +0100

    implt. full pegasos algorith

--- projects/sentiment_analysis/project1.py
+++ projects/sentiment_analysis/project1.py
@@ -246,7 +246,18 @@ def pegasos(feature_matrix, labels, T, L):
     parameter, found after T iterations through the feature matrix.
     """
     # Your code here
-    raise NotImplementedError
+    number_features = feature_matrix.shape[1]
+    number_points = feature_matrix.shape[0]
+    theta = np.zeros(number_features)
+    theta_0 = 0
+    t = 0
+    for j in range(T):
+        for i in get_order(number_points):
+            # Your code here
+            t = t + 1
+            eta = 1/(np.sqrt(t))
+            theta, theta_0 = pegasos_single_step_update(feature_matrix[i], labels[i], L, eta, theta, theta_0)
+    return theta, theta_0
 
 # Part II
 
