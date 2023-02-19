b8044e402c12325d28683fc4fb3d776710e3a347
Beatriz Negreiros (IWS) <beatriznegreiros@outlook.com>
2023-02-19 14:25:46 +0100

    solved aram. tuning and test acc.

--- projects/sentiment_analysis/main.py
+++ projects/sentiment_analysis/main.py
@@ -6,41 +6,41 @@ import numpy as np
 # Data loading. There is no need to edit code in this section.
 #-------------------------------------------------------------------------------
 
-# train_data = utils.load_data('reviews_train.tsv')
-# val_data = utils.load_data('reviews_val.tsv')
-# test_data = utils.load_data('reviews_test.tsv')
-#
-# train_texts, train_labels = zip(*((sample['text'], sample['sentiment']) for sample in train_data))
-# val_texts, val_labels = zip(*((sample['text'], sample['sentiment']) for sample in val_data))
-# test_texts, test_labels = zip(*((sample['text'], sample['sentiment']) for sample in test_data))
-#
-# dictionary = p1.bag_of_words(train_texts)
-#
-# train_bow_features = p1.extract_bow_feature_vectors(train_texts, dictionary)
-# val_bow_features = p1.extract_bow_feature_vectors(val_texts, dictionary)
-# test_bow_features = p1.extract_bow_feature_vectors(test_texts, dictionary)
+train_data = utils.load_data('reviews_train.tsv')
+val_data = utils.load_data('reviews_val.tsv')
+test_data = utils.load_data('reviews_test.tsv')
+
+train_texts, train_labels = zip(*((sample['text'], sample['sentiment']) for sample in train_data))
+val_texts, val_labels = zip(*((sample['text'], sample['sentiment']) for sample in val_data))
+test_texts, test_labels = zip(*((sample['text'], sample['sentiment']) for sample in test_data))
+
+dictionary = p1.bag_of_words(train_texts)
+
+train_bow_features = p1.extract_bow_feature_vectors(train_texts, dictionary)
+val_bow_features = p1.extract_bow_feature_vectors(val_texts, dictionary)
+test_bow_features = p1.extract_bow_feature_vectors(test_texts, dictionary)
 
 #-------------------------------------------------------------------------------
 # Problem 5
 #-------------------------------------------------------------------------------
 
-toy_features, toy_labels = toy_data = utils.load_toy_data('toy_data.tsv')
-
-T = 100
-L = 0.2
-
-thetas_perceptron = p1.perceptron(toy_features, toy_labels, T)
-thetas_avg_perceptron = p1.average_perceptron(toy_features, toy_labels, T)
-thetas_pegasos = p1.pegasos(toy_features, toy_labels, T, L)
-
-def plot_toy_results(algo_name, thetas):
-    print('theta for', algo_name, 'is', ', '.join(map(str,list(thetas[0]))))
-    print('theta_0 for', algo_name, 'is', str(thetas[1]))
-    utils.plot_toy_data(algo_name, toy_features, toy_labels, thetas)
-
-plot_toy_results('Perceptron', thetas_perceptron)
-plot_toy_results('Average Perceptron', thetas_avg_perceptron)
-plot_toy_results('Pegasos', thetas_pegasos)
+# toy_features, toy_labels = toy_data = utils.load_toy_data('toy_data.tsv')
+#
+# T = 100
+# L = 0.2
+#
+# thetas_perceptron = p1.perceptron(toy_features, toy_labels, T)
+# thetas_avg_perceptron = p1.average_perceptron(toy_features, toy_labels, T)
+# thetas_pegasos = p1.pegasos(toy_features, toy_labels, T, L)
+#
+# def plot_toy_results(algo_name, thetas):
+#     print('theta for', algo_name, 'is', ', '.join(map(str,list(thetas[0]))))
+#     print('theta_0 for', algo_name, 'is', str(thetas[1]))
+#     utils.plot_toy_data(algo_name, toy_features, toy_labels, thetas)
+#
+# plot_toy_results('Perceptron', thetas_perceptron)
+# plot_toy_results('Average Perceptron', thetas_avg_perceptron)
+# plot_toy_results('Pegasos', thetas_pegasos)
 
 #-------------------------------------------------------------------------------
 # Problem 7
@@ -68,9 +68,9 @@ plot_toy_results('Pegasos', thetas_pegasos)
 # Problem 8
 #-------------------------------------------------------------------------------
 
-# data = (train_bow_features, train_labels, val_bow_features, val_labels)
-#
-# # values of T and lambda to try
+data = (train_bow_features, train_labels, val_bow_features, val_labels)
+
+# values of T and lambda to try
 # Ts = [1, 5, 10, 15, 25, 50]
 # Ls = [0.001, 0.01, 0.1, 1, 10]
 #
@@ -105,7 +105,9 @@ plot_toy_results('Pegasos', thetas_pegasos)
 # test_bow_features and test_labels.
 #-------------------------------------------------------------------------------
 
-# Your code here
+acc_train, acc_test = p1.classifier_accuracy(p1.pegasos, train_bow_features, test_bow_features, train_labels,
+                                                test_labels, T=25, L=0.0100)
+print(acc_train, acc_test)
 
 #-------------------------------------------------------------------------------
 # Assign to best_theta, the weights (and not the bias!) learned by your most
