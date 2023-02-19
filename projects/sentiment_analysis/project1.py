240a8284e0889ab740d46a16d942a3c45802214a
Beatriz Negreiros (IWS) <beatriznegreiros@outlook.com>
2023-02-19 15:16:10 +0100

    fin. problem 9

--- projects/sentiment_analysis/project1.py
+++ projects/sentiment_analysis/project1.py
@@ -384,7 +384,7 @@ def extract_bow_feature_vectors(reviews, dictionary):
         word_list = extract_words(text)
         for word in word_list:
             if word in dictionary:
-                feature_matrix[i, dictionary[word]] = 1
+                feature_matrix[i, dictionary[word]] += 1
     return feature_matrix
 
 
