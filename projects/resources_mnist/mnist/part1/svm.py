58006630fe81dc5cae3f67ac21d1257470c760c2
Beatriz Negreiros (IWS) <beatriznegreiros@outlook.com>
2023-02-28 18:52:46 +0100

    solved multiclass class.

--- projects/resources_mnist/mnist/part1/svm.py
+++ projects/resources_mnist/mnist/part1/svm.py
@@ -15,7 +15,7 @@ def one_vs_rest_svm(train_x, train_y, test_x):
     Returns:
         pred_test_y - (m,) NumPy array containing the labels (0 or 1) for each test data point
     """
-    model = LinearSVC(random_state=0, C=1)
+    model = LinearSVC(random_state=0, C=0.1)
     model.fit(train_x, train_y)
     return model.predict(test_x)
 
@@ -32,7 +32,9 @@ def multi_class_svm(train_x, train_y, test_x):
     Returns:
         pred_test_y - (m,) NumPy array containing the labels (int) for each test data point
     """
-    raise NotImplementedError
+    model = LinearSVC(random_state=0, C=0.1)
+    model.fit(train_x, train_y)
+    return model.predict(test_x)
 
 
 def compute_test_error_svm(test_y, pred_test_y):
